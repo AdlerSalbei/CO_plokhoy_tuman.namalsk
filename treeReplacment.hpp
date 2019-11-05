2b3d019b2d88cf73c5d79637652de752efe3bfa7
Salbei <salbei@gruppe-adler.de>
2019-11-05 23:36:13 +0100

    starting replacement of trees

---
+++ treeReplacment.hpp
@@ -0,0 +1,44 @@
+class treeReplacement {
+	class t_picea1s_w {
+
+	};
+	class z_pinus_mugo_lowpoly {
+
+	};
+	class misc_trunk_water {
+
+	};
+	class t_picea2s_w {
+
+	};
+	class t_picea2s {
+
+	};
+	class t_picea1s {
+
+	};
+	class t_picea3f {
+
+	};
+	class misc_stub1 {
+
+	};
+	class seb_spadly_stozar {
+
+	};
+	class pinus_mugo_lowpoly {
+
+	};
+	class t_pinusn1s {
+
+	};
+	class t_pinusn2s {
+
+	};
+	class t_pinuss2f {
+
+	};
+};
+
+
+
